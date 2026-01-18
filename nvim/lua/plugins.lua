1cd79dcc3e93830442b0731bd259d7ceff25ec35
zpeng11 <a2843173630@gmail.com>
2026-01-18 16:33:23 -0500

    feat: update experience

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -30,4 +30,30 @@ require("lazy").setup({
       { "<leader>u", "<cmd>UndotreeToggle<CR>" },
     },
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup({
+        -- 这里的配置可以控制标注的样式
+        signs = {
+          add          = { text = '┃' },
+          change       = { text = '┃' },
+          delete       = { text = '_' },
+          topdelete    = { text = '‾' },
+          changedelete = { text = '~' },
+          untracked    = { text = '┆' },
+        },
+        signcolumn = true,  -- 开启侧边栏符号
+        numhl      = true, -- 开启行号高亮 (可选)
+        linehl     = false, -- 开启整行高亮 (可选，开启后非常明显)
+        word_diff  = false, -- 开启行内具体修改单词的高亮 (可选)
+        watch_gitdir = {
+          interval = 1000,
+          follow_files = true
+        },
+        attach_to_untracked = true,
+        current_line_blame = true, -- 开启行末 Git Blame 信息 (非常推荐)
+      })
+    end
+  }
 })
